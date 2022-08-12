f312c140315e9c878fd18320f576102d8710b293
Raquel Jones <tjones7511@gmail.com>
2022-08-12 09:09:45 -0500

    JS day 1 ex

---
+++ app.js
@@ -0,0 +1,15 @@
+const name = "Raquel"
+let last name = "jones"
+let language = "JavaScript"
+let createdYear = 1999
+let price = 19.99j
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let boodmark = 456
+let hasRead = true
